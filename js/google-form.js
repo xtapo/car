464a1173c7b822999c557fef8176ed8b2cdd8357
xtapo <40712625+xtapo@users.noreply.github.com>
2018-07-10 10:20:13 +0700

    update

---
+++ js/google-form.js
@@ -0,0 +1,52 @@
+// This script requires jQuery and jquery-form plugin
+// You can use these ones from Cloudflare CDN:
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js" integrity="sha256-2Pjr1OlpZMY6qesJM68t2v39t+lMLvxwpa8QlRjJroA=" crossorigin="anonymous"></script>
+//
+$('#bootstrapForm').submit(function (event) {
+    event.preventDefault()
+    var extraData = {}
+    {
+        /* Parsing input date id=119521239 */
+        var dateField = $("#119521239_date").val()
+        var timeField = $("#119521239_time").val()
+        let d = new Date(dateField)
+        if (!isNaN(d.getTime())) {
+            extraData["entry.119521239_year"] = d.getFullYear()
+            extraData["entry.119521239_month"] = d.getMonth() + 1
+            extraData["entry.119521239_day"] = d.getUTCDate()
+        }
+        if (timeField && timeField.split(':').length >= 2) {
+            let values = timeField.split(':')
+            extraData["entry.119521239_hour"] = values[0]
+            extraData["entry.119521239_minute"] = values[1]
+        }
+    }
+    {
+        /* Parsing input date id=104276851 */
+        var dateField = $("#104276851_date").val()
+        var timeField = $("#104276851_time").val()
+        let d = new Date(dateField)
+        if (!isNaN(d.getTime())) {
+            extraData["entry.104276851_year"] = d.getFullYear()
+            extraData["entry.104276851_month"] = d.getMonth() + 1
+            extraData["entry.104276851_day"] = d.getUTCDate()
+        }
+        if (timeField && timeField.split(':').length >= 2) {
+            let values = timeField.split(':')
+            extraData["entry.104276851_hour"] = values[0]
+            extraData["entry.104276851_minute"] = values[1]
+        }
+    }
+    $('#bootstrapForm').ajaxSubmit({
+        data: extraData,
+        dataType: 'jsonp',  // This won't really work. It's just to use a GET instead of a POST to allow cookies from different domain.
+        error: function () {
+            // Submit of form should be successful but JSONP callback will fail because Google Forms
+            // does not support it, so this is handled as a failure.
+            alert('Form Submitted. Thanks.')
+            // You can also redirect the user to a custom thank-you page:
+            // window.location = 'http://www.mydomain.com/thankyoupage.html'
+        }
+    })
+})
